a47927c230954b12e788b76da3c43cfecdf604a3
phtrebil <phptrebil@gmail.com>
2023-05-30 16:31:19 -0300

    primeiro dia do desafio concluido, criada primeira tela com jetpack compose

--- app/src/main/java/com/example/devhub/MainActivity.kt
+++ app/src/main/java/com/example/devhub/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +26,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TelaDePerfil()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun TelaDePerfil() {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val profileImage = painterResource(R.drawable.perfil)
+            Image(
+                painter = profileImage,
+                contentDescription = "Foto do Perfil",
+                modifier = Modifier.size(120.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Pedro Henrique Perez Trebilcock", style = MaterialTheme.typography.h6)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "@phtrebil", style = MaterialTheme.typography.body1)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Desenvolvedor Android apaixonado por tecnologia e programação.",
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DevHubTheme {
-        Greeting("Android")
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        DevHubTheme {
+            TelaDePerfil()
+        }
     }
 }
