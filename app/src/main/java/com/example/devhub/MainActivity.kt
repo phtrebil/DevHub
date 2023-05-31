6ba8fad2725df8f568f13a387856902720fbbda2
phtrebil <phptrebil@gmail.com>
2023-05-31 10:34:13 -0300

    aperfeiçoando habilidades com compose e melhorando tela

--- app/src/main/java/com/example/devhub/MainActivity.kt
+++ app/src/main/java/com/example/devhub/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,26 +41,54 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun TelaDePerfil() {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            val profileImage = painterResource(R.drawable.perfil)
-            Image(
-                painter = profileImage,
-                contentDescription = "Foto do Perfil",
-                modifier = Modifier.size(120.dp)
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "Pedro Henrique Perez Trebilcock", style = MaterialTheme.typography.h6)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "@phtrebil", style = MaterialTheme.typography.body1)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Desenvolvedor Android apaixonado por tecnologia e programação.",
-                style = MaterialTheme.typography.body2
-            )
+        Column {
+            val boxAltura = remember {
+                150.dp
+            }
+            val alturaImagem = remember {
+                boxAltura
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        Color(0xFF2d333b), shape = RoundedCornerShape(
+                            bottomStart = 16.dp,
+                            bottomEnd = 16.dp
+                        )
+                    )
+                    .height(boxAltura)
+            ) {
+                val profileImage = painterResource(R.drawable.perfil)
+                Image(
+                    painter = profileImage,
+                    contentDescription = "Foto do Perfil",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .offset(y = alturaImagem/2)
+                        .size(alturaImagem)
+                        .align(Alignment.BottomCenter)
+                        .clip(CircleShape)
+                )
+            }
+            Spacer(modifier = Modifier.height(alturaImagem / 2))
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Pedro Henrique Perez Trebilcock", style = MaterialTheme.typography.h6)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "@phtrebil", style = MaterialTheme.typography.body1)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Desenvolvedor Android apaixonado por tecnologia e programação.",
+                    style = MaterialTheme.typography.body2
+                )
+            }
+
+
         }
     }
 
